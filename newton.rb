0eadc4eae3cf4d81afeb8cbe28050f13f5383fd5
Artem Batrakov <leetim@live.ru>
2016-05-13 00:46:25 +1000

    Написал Расчет погрешностей

--- newton.rb
+++ newton.rb
@@ -4,6 +4,10 @@ def f(x)
 	x**2 + Math::log10(x + 2)
 end
 
+def f_10(x)
+	-362880/(x + 2)**10
+end
+
 $a = 0.5
 $b = 1.0
 x1 = 0.53
@@ -15,10 +19,20 @@ $xi = Array.new(11){|i| $a + i * $h}
 $fi = Array.new(11) {|i| f($xi[i])}
 
 class Array
+	#Cумма
 	def sum()
 		a = 0
 		for i in (0...self.length)
 			a += self[i]
+		end
+		a
+	end
+
+	#Произведение
+	def pr()
+		a = 0
+		for i in (0...self.length)
+			a *= self[i]
 		end
 		a
 	end
@@ -51,6 +65,13 @@ def fact_t(t, k)
 	(t - k) * fact_t(t, k - 1)
 end
 
+#погрешность
+def R(fnk, x, k)
+	a = Array.new(11) {|i| x - $xi[i]}
+	# p a
+	fnk * a.pr / fact(k)
+end
+
 #формула Ньютона
 def L_n(x)
 	t = (x - $a) / $h
@@ -58,10 +79,15 @@ def L_n(x)
 	a.sum()
 end
 
-p $xi
-p $fi 
-
-p L_n(x1) - f(x1)
-p L_n(x2) - f(x2)
-p L_n(x3) - f(x3)
-p L_n(x4) - f(x4)
+puts "R: #{L_n(x1) - f(x1)}"
+puts "R_min: #{R(f_10($a), x1, 10)}"
+puts "R_max: #{R(f_10($b), x1, 10)}"
+puts "R: #{L_n(x2) - f(x2)}"
+puts "R_min: #{R(f_10($a), x2, 10)}"
+puts "R_max: #{R(f_10($b), x2, 10)}"
+puts "R: #{L_n(x3) - f(x3)}"
+puts "R_min: #{R(f_10($a), x3, 10)}"
+puts "R_max: #{R(f_10($b), x3, 10)}"
+puts "R: #{L_n(x4) - f(x4)}"
+puts "R_min: #{R(f_10($a), x4, 10)}"
+puts "R_max: #{R(f_10($b), x4, 10)}"
